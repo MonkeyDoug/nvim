dc57c39160299d429dcbd4e473a13c02eee33f44
Douglas Chen <dchen3@andrew.cmu.edu>
2026-01-09 10:58:56 -0500

    do accepted instead of ignored ft for tree sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,17 +1,22 @@
 require("nvim-treesitter").install({ "python", "rust", "bash", "lua", "latex", "vim", "toml" })
+
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(args)
-		local ignored_ft = {
-			"latex",
-			"help",
-			"dashboard",
-			"dashboardpreview",
-			"TelescopePrompt",
-			"TelescopeResults",
-			"minifiles",
-			"text",
+		-- List of filetypes you want to ENABLE
+		local accepted_ft = {
+			"python",
+			"rust",
+			"bash",
+			"sh",
+			"lua",
+			"tex",
+			"vim",
+			"toml",
 		}
-		if vim.tbl_contains(ignored_ft, vim.bo[args.buf].filetype) then
+
+		-- Check if the current filetype is in the accepted list
+		-- If it is NOT in the list, we return (stop)
+		if not vim.tbl_contains(accepted_ft, vim.bo[args.buf].filetype) then
 			return
 		end
 
